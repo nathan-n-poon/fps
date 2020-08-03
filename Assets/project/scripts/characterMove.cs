dc7600884fc469a2714b4dd6877aea1f01e17811
nathan-n-poon <nathannpoon@gmail.com>
2020-08-03 00:07:03 -0600

    oink

---
+++ Assets/project/scripts/characterMove.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMove : MonoBehaviour
+{
+    public Rigidbody m_Rigidbody;
+    public Collider Collider;
+
+    public float maxSpeed = 4f;
+    public float walkAccel = 1f;
+    public float gravity = -10f;
+    public float skinDepth = 0.3f;
+
+
+    Vector3 speed;
+    float speedX;
+    float speedY;
+    float speedZ;
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //
+        //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //x = Input.GetAxis("Horizontal");
+        //z = Input.GetAxis("Vertical");
+        //Vector3 move = transform.right * x + transform.forward * z;
+
+        //isGrounded = checkGround();
+        ////deltaY.y += gravity * Time.deltaTime;
+        //if (isGrounded)
+        //{
+        //    deltaY.y = 0;
+        //}
+
+        //m_Rigidbody.MovePosition(m_Rigidbody.position + (move + deltaY) * Speed * Time.deltaTime);
+
+        //--------------------------------------------------------------------------------------------------
+
+
+        //dx = (v + a/2*t) * t
+        Vector3 speed = Vector3.zero;
+
+        speedX = accelerate("Horizontal");
+        //Debug.Log("speedX is: ");
+        Debug.Log(speedX);
+        speedZ = accelerate("Vertical");
+
+        checkCollisions();
+
+        speed = transform.right * speedX + transform.forward * speedZ;
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + (speed) * Time.deltaTime);
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+    float accelerate(string axis)
+    {
+        float velocity = (axis == "Horizontal") ? speedX : speedZ;
+        float magnitude = Mathf.Abs(velocity);
+        float previousDirection = 0;
+        float accel = walkAccel * Time.deltaTime;
+
+        if(magnitude != 0)
+        {
+            previousDirection = velocity / magnitude;
+        }
+
+        if (Input.GetAxisRaw(axis) != 0 && Input.GetAxisRaw(axis) != -previousDirection)
+        {
+            magnitude += accel;
+        }
+        else
+        {
+            magnitude -= accel;
+        }
+        magnitude = Mathf.Clamp(magnitude, 0f, maxSpeed);
+        //if (Input.GetAxisRaw(axis) != 0)
+        //{
+        //    magnitude *= Input.GetAxisRaw(axis);
+        //}
+        if (velocity != 0)
+        {
+            magnitude *= previousDirection;
+        }
+        return magnitude;
+    }
+
+    void checkCollisions()
+    {
+        //float DisstanceToTheGround = Collider.bounds.extents.y;   
+        //return Physics.Raycast(transform.position, Vector3.down, DisstanceToTheGround + 0.1f);
+        float xDistance = Collider.bounds.extents.x + skinDepth;
+        float yDistance = Collider.bounds.extents.y + skinDepth;
+        float zDistance = Collider.bounds.extents.z + skinDepth;
+        if (Physics.Raycast(transform.position, Vector3.right, xDistance) || Physics.Raycast(transform.position, Vector3.left, xDistance))
+        {
+            Debug.Log("Forwards/Backwards");
+            speedX = 0;
+        }
+        if (Physics.Raycast(transform.position, Vector3.down, yDistance) || Physics.Raycast(transform.position, Vector3.up, yDistance))
+        {
+            Debug.Log("Top/Bottom");
+            speedY = 0;
+        }
+        if (Physics.Raycast(transform.position, Vector3.forward, zDistance) || Physics.Raycast(transform.position, Vector3.back, zDistance))
+        {
+            Debug.Log("Right/Left");
+            speedZ = 0;
+        }
+    }
+}
---
+++ Assets/project/scripts/mouseLook.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseLook : MonoBehaviour
+{
+    //Make sure you attach a Rigidbody in the Inspector of this GameObject
+    public Rigidbody m_Rigidbody;
+
+    Quaternion deltaRotation;
+    float xRotation = 0f;
+
+    public float mouseXSensitivity = 100f;
+    public float mouseYSensitivity = 100f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //Fetch the Rigidbody from the GameObject with this script attached
+        //m_Rigidbody = GetComponent<Rigidbody>();
+
+        deltaRotation = new Quaternion();
+    }
+
+    private void Update()
+    {
+        //rotation
+        float mouseX = Input.GetAxis("Mouse X") * mouseXSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseYSensitivity * Time.deltaTime;
+
+        deltaRotation = Quaternion.Euler(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //valid--------------------
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        //-----------------------------------------------------
+
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
