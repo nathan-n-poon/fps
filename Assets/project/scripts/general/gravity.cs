478362efcc39e190b37dc0457c87fc95b10dc5c3
nathanPoon <nathannpoon@gmail.com>
2020-09-01 23:50:59 -0600

    uwu

--- Assets/project/scripts/general/gravity.cs
+++ Assets/project/scripts/general/gravity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class general : MonoBehaviour
+public class gravity
 {
     // Start is called before the first frame update
     void Start()
