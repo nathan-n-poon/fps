49780da15299b2c9448ee8f175069e6daf68798a
nathanPoon <nathannpoon@gmail.com>
2020-09-03 19:16:07 -0600

    owo

---
+++ Assets/project/scripts/Model Scripts/accelerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class accelerator
+{
+    protected float previousVelocity;
+    protected float previousDirection;
+
+    protected float currentDirection;
+
+    protected float walkAccel;
+    protected float maxSpeed;
+    protected float accel;
+
+    public abstract float move(float previousSpeed, float currentDirection, bool isFloored);
+
+    public abstract void update(float previousSpeed);
+
+    public abstract float accelerate(float currentDirection, float previousVelocity, bool isFloored);
+
+    public abstract float decelerate(float previousVelocity);
+}
---
+++ Assets/project/scripts/Model Scripts/walkingAccelerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkingAccelerator : accelerator
+{
+    public walkingAccelerator()
+    {
+        this.walkAccel = 3f;
+        this.maxSpeed = 6f;
+    }
+
+    public override float move(float previousSpeed, float currentDirection, bool isFloored)
+    {
+        float newVelocity;
+        update(previousSpeed);
+        newVelocity = accelerate(currentDirection, previousVelocity, isFloored);
+        newVelocity = decelerate(newVelocity);
+        return newVelocity;
+    }
+
+    public override void update(float previousSpeed)
+    {
+        accel = walkAccel* Time.deltaTime;
+        previousVelocity = previousSpeed;
+        previousDirection = Mathf.Sign(previousVelocity);
+    }
+
+    public override float accelerate(float currentDirection, float previousVelocity, bool isFloored)
+    {
+        float newVelocity = previousVelocity;
+        this.currentDirection = currentDirection;
+        if (currentDirection != 0 && isFloored)
+        {
+            newVelocity = previousVelocity + accel * currentDirection;
+            if (Mathf.Abs(newVelocity) > maxSpeed)
+            {
+                newVelocity = maxSpeed * currentDirection;
+            }
+        }
+        return newVelocity;
+    }
+
+    public override float decelerate(float previousVelocity)
+    {
+        float newVelocity = previousVelocity;
+        //if (GameObject.FindObjectOfType<characterMove>().getIsFloored())
+        //{
+        if (Mathf.Sign(previousVelocity - accel * previousDirection / 1.5f) == Mathf.Sign(previousVelocity))
+        {
+            newVelocity -= accel * previousDirection / 1.5f;
+        }
+        else
+            newVelocity = 0;
+
+        return newVelocity;
+        //}
+    }
+
+}
