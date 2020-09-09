be7523e5de6e079e0e6149009e6ce4037788a4c6
nathanPoon <nathannpoon@gmail.com>
2020-09-09 14:53:14 -0600

    not working

---
+++ Assets/project/scripts/Model Scripts/launcher.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class launcher : MonoBehaviourPunCallbacks
+{
+    public void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        connect();
+    }
+
+    public void connect()
+    {
+        PhotonNetwork.GameVersion = "0";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        join();
+        base.OnConnectedToMaster();
+    }
+
+    public void join()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        startGame();
+        base.OnJoinedRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        create();
+        base.OnJoinRandomFailed(returnCode, message);
+    }
+
+    public void create()
+    {
+        PhotonNetwork.CreateRoom("placeholder");
+    }
+
+    public void startGame()
+    {
+        if(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            PhotonNetwork.LoadLevel(1);
+        }
+    }
+}
---
+++ Assets/project/scripts/Model Scripts/manager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class manager : MonoBehaviour
+{
+    public string playerPrefab;
+
+    private void Start()
+    {
+        spawn();
+    }
+
+    public void spawn()
+    {
+        PhotonNetwork.Instantiate()
+    }
+}
